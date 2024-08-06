3e3866882d3b0bd96da6757096b492ee174ee0a6
Taka <takahito.uchino.1218@gmail.com>
2024-08-06 20:57:19 +0900

    [add]REST API作成
    TodoのCRUD処理を実装しました。

--- backend/main.go
+++ backend/main.go
@@ -1,25 +1,20 @@
 package main
 
 import (
-	"net/http"
-
+	"github.com/Takahito-Uchino/Go-React-Todo/controller"
 	"github.com/Takahito-Uchino/Go-React-Todo/model"
 	"github.com/labstack/echo/v4"
 )
 
-func connect(c echo.Context) error {
-	db, _ := model.DB.DB()
-	defer db.Close()
-	err := db.Ping()
-	if err != nil {
-		return c.String(http.StatusInternalServerError, "DB接続失敗しました")
-	} else {
-		return c.String(http.StatusOK, "DB接続しました")
-	}
-}
-
 func main() {
 	e := echo.New()
-	e.GET("/", connect)
+	db, _ := model.DB.DB()
+	defer db.Close()
+
+	e.GET("/todos", controller.GetTodos)
+	e.GET("/todos/:id", controller.GetTodo)
+	e.POST("/todos", controller.CreateTodo)
+	e.PUT("/todos/:id", controller.UpdateTodo)
+	e.DELETE("/todos/:id", controller.DeleteTodo)
 	e.Logger.Fatal(e.Start(":8080"))
 }
